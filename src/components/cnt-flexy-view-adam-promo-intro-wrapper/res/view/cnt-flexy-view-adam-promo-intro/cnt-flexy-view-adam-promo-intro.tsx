49650e63af88102771e64f937a7f0a8d7313a529
freeside <almalib@bk.ru>
2020-11-13 10:55:34 +0300

    adden social links

--- src/components/cnt-flexy-view-adam-promo-intro-wrapper/res/view/cnt-flexy-view-adam-promo-intro/cnt-flexy-view-adam-promo-intro.tsx
+++ src/components/cnt-flexy-view-adam-promo-intro-wrapper/res/view/cnt-flexy-view-adam-promo-intro/cnt-flexy-view-adam-promo-intro.tsx
@@ -1,11 +1,11 @@
-import { Component, ComponentInterface, h, Prop } from '@stencil/core';
-import {PromoIntroBlockInterface} from "./res/interface/common.interface";
+import { Component, ComponentInterface, h, Prop } from "@stencil/core";
+import { PromoIntroBlockInterface } from "./res/interface/common.interface";
 
 @Component({
-  tag: 'cnt-flexy-view-adam-promo-intro',
-  styleUrl: 'cnt-flexy-view-adam-promo-intro.css',
+  tag: "cnt-flexy-view-adam-promo-intro",
+  styleUrl: "cnt-flexy-view-adam-promo-intro.css",
   shadow: false,
-  scoped: true
+  scoped: true,
 })
 export class CntFlexyViewAdamPromoIntro implements ComponentInterface {
   /**
@@ -15,12 +15,17 @@ export class CntFlexyViewAdamPromoIntro implements ComponentInterface {
 
   render() {
     return (
-      <header class="main-header" style={{backgroundImage: `url(${this.payload.img})`}} >
+      <header
+        class="main-header"
+        style={{ backgroundImage: `url(${this.payload.img})` }}
+      >
         <div class="container">
           <div class="row personal-profile">
             <div class="col-md-4 personal-profile-avatar-wrapper">
-              <div class="personal-profile-avatar" style={{backgroundImage: `url(${this.payload.img})`}}>
-              </div>
+              <div
+                class="personal-profile-avatar"
+                style={{ backgroundImage: `url(${this.payload.img})` }}
+              ></div>
             </div>
             <div class="col-md-8">
               <p class="personal-profile-name">{this.payload.name}</p>
@@ -30,24 +35,33 @@ export class CntFlexyViewAdamPromoIntro implements ComponentInterface {
                   <dt>Возраст:</dt>
                   <dd>{this.payload.age}</dd>
                   <dt>Телефон:</dt>
-                  <dd><a href={'tel:'+this.payload.phone+''}>{this.payload.phone}</a></dd>
+                  <dd>
+                    <a href={"tel:" + this.payload.phone + ""}>
+                      {this.payload.phone}
+                    </a>
+                  </dd>
                   <dt>Почта:</dt>
-                  <dd><a href={'mailto:'+this.payload.mail+''}>{this.payload.mail}</a></dd>
+                  <dd>
+                    <a href={"mailto:" + this.payload.mail + ""}>
+                      {this.payload.mail}
+                    </a>
+                  </dd>
                   <dt>Адрес:</dt>
                   <dd>{this.payload.address}</dd>
                 </dl>
               </div>
               <p class="personal-profiles-social">
-                <a href=""><i class="fab fa-github"></i></a>
-                <a href=""><i class="fab fa-instagram-square"></i></a>
-                <a href=""><i class="fab fa-facebook-square"></i></a>
+                <a href="https://github.com/almalib/ng-promo">
+                  <i class="fab fa-github"></i>
+                </a>
+                <a href="https://www.instagram.com/free_side/">
+                  <i class="fab fa-instagram-square"></i>
+                </a>
               </p>
             </div>
           </div>
         </div>
       </header>
-
     );
   }
-
 }
